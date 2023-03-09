d96a7fe68739c395229d92c56c81ba239fb80b28
”Mechapa” <Jordas5@mail.ru>
2023-03-10 00:16:37 +0300

    first commit fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,21 @@
 // TODO: write your code here
 const characterStats = {
-    name: 'Маг',
-    health: '90',
-  };
-  
-  export default function checkHealthStatus(data) {
-    let healthStatus;
-    if (data.health > 50 && data.health <= 100) {
-      healthStatus = 'healthy';
-    }
-    if (data.health <= 50 && data.health >= 15) {
-      healthStatus = 'wounded';
-    }
-    if (data.health < 15 && data.health > 0) {
-      healthStatus = 'critical';
-    }
-    return healthStatus;
+  name: 'Маг',
+  health: '90',
+};
+
+export default function checkHealthStatus(data) {
+  let healthStatus;
+  if (data.health > 50 && data.health <= 100) {
+    healthStatus = 'healthy';
   }
-  
-  checkHealthStatus(characterStats);
+  if (data.health <= 50 && data.health >= 15) {
+    healthStatus = 'wounded';
+  }
+  if (data.health < 15 && data.health > 0) {
+    healthStatus = 'critical';
+  }
+  return healthStatus;
+}
+
+checkHealthStatus(characterStats);
